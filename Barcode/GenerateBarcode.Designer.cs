55f6ed382a28f96288eed5b1e82bf679a51447e9
duygu saygın <duygu_saygin_96@hotmail.com>
2020-06-01 11:34:57 +0300

    deneme

---
+++ Barcode/GenerateBarcode.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Barcode
+{
+    partial class GenerateBarcode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(474, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Genarate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(70, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(373, 34);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(81, 151);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(459, 358);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(70, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(373, 34);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(474, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 48);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "qrcode";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(649, 214);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 48);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Print";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(649, 97);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(169, 48);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "qrcode2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(649, 151);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(169, 48);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "qrcode3";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(649, 277);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(137, 48);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Decode";
+            this.button6.UseVisualStyleBackColor = true;
+           
+            // 
+            // GenerateBarcode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 541);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "GenerateBarcode";
+            this.Text = "GenerateBarcode";
+            this.Load += new System.EventHandler(this.GenerateBarcode_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
---
+++ Barcode/GenerateBarcode.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using  System.IO;
+using  System.Drawing.Imaging;
+using System.Drawing.Printing;
+using QRCoder;
+using BarcodeLib;
+using ZXing;
+
+namespace Barcode
+{
+    public partial class GenerateBarcode : Form
+    {
+        public GenerateBarcode()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Zen.Barcode.Code128BarcodeDraw barcode = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+            pictureBox1.Image = barcode.Draw(textBox1.Text,50);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Zen.Barcode.CodeQrBarcodeDraw qrcode = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+         
+            pictureBox1.Image = qrcode.Draw(textBox2.Text, 50);
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e) => printBarCode();
+
+        private void printBarCode()
+        {
+            PrintDialog pd=new PrintDialog();
+            PrintDocument doc=new PrintDocument();
+            doc.PrintPage += Doc_PrintPage;
+            pd.Document = doc;
+            if (pd.ShowDialog()==DialogResult.OK)
+            {
+                doc.Print();
+            }
+        }
+
+        private void Doc_PrintPage(object sender, PrintPageEventArgs e)
+        {
+          Bitmap bm=new Bitmap(pictureBox1.Width, pictureBox1.Height);
+          float scale= Math.Min(300 / bm.Width,300/ bm.Height);
+          var scaleWidth = (int)(bm.Width * scale);
+          var scaleHeight = (int)(bm.Height * scale);
+            //pictureBox1.DrawToBitmap(bm, new Rectangle(50 * pictureBox1.Width / pictureBox1.Height, 50 * pictureBox1.Height / pictureBox1.Width, ((int)150 - scaleWidth) / 2, ((int)150 - scaleHeight) / 2));
+            pictureBox1.DrawToBitmap(bm, new Rectangle(30 * pictureBox1.Width / pictureBox1.Height, 30 * pictureBox1.Height / pictureBox1.Width, pictureBox1.Width, pictureBox1.Height));
+            pictureBox1.DrawToBitmap(bm, new Rectangle(30 * pictureBox1.Width / pictureBox1.Height, 30 * pictureBox1.Height / pictureBox1.Width, pictureBox1.Width, pictureBox1.Height));
+            pictureBox1.DrawToBitmap(bm, new Rectangle(30 * pictureBox1.Width / pictureBox1.Height, 30 * pictureBox1.Height / pictureBox1.Width, pictureBox1.Width, pictureBox1.Height));
+            e.Graphics.DrawImage(bm,0,0);
+          bm.Dispose();
+        }
+
+        private void GenerateBarcode_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            QRCoder.QRCodeGenerator qrCodeGenerator=new QRCodeGenerator();
+            var qrData = qrCodeGenerator.CreateQrCode(textBox2.Text, QRCoder.QRCodeGenerator.ECCLevel.Q);
+            var qrCode=new QRCoder.QRCode(qrData);
+            var image = qrCode.GetGraphic(2);
+            //pictureBox1.Image = image;
+            Bitmap b = new Bitmap(pictureBox1.Width, pictureBox1.Height, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(b);
+            g.DrawImage(Image.FromFile(@"C:\pencere.bmp"),110,5, Image.FromFile(@"C:\pencere.bmp").Width, Image.FromFile(@"C:\pencere.bmp").Height);
+            g.DrawImage(image, 0, 0, image.Width , image.Height);
+            using (Font myFont = new Font("Arial", 14))
+            {
+                g.DrawString("Hello!", myFont, Brushes.Green, new Point(20, 130));
+            }
+            //Pen pen = new Pen(Color.Black, 1);
+            //pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+            //g.DrawRectangle(pen, 0, 0, pictureBox1.Width, pictureBox1.Height);
+
+            pictureBox1.Image = b;
+
+            //QRCoder.QRCodeGenerator qrCodeGenerator = new QRCodeGenerator();
+            //var qrData = qrCodeGenerator.CreateQrCode(textBox2.Text, QRCoder.QRCodeGenerator.ECCLevel.Q);
+            //var qrCode = new QRCoder.QRCode(qrData);
+            //var image = qrCode.GetGraphic(1);
+            //pictureBox1.Image = image;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            QRCoder.QRCodeGenerator qrCodeGenerator = new QRCodeGenerator();
+            var qrData = qrCodeGenerator.CreateQrCode(textBox2.Text, QRCoder.QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCoder.QRCode(qrData);
+            var image = qrCode.GetGraphic(2);
+            pictureBox1.Image = image;
+        }
+
+       
+    }
+}
